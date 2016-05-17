b8c98fcd15c24d846cb426129161ea35e56ca618
Cristiano Cucco <cristiano.cucco@gmail.com>
2016-05-17 17:32:53 +0200

    fixed unset conflict

--- src/Command/Handler.php
+++ src/Command/Handler.php
@@ -2,6 +2,7 @@
 
 namespace Gr77\Command;
 
+use Gr77\Session\Session;
 use Gr77\Telegram\Client;
 use Gr77\Telegram\Update;
 use Psr\Log\LoggerInterface;
@@ -10,9 +11,13 @@ interface Handler
 {
     /**
      * Returns Handler concrete instance
+     * @param Client $client
+     * @param Session $session
+     * @param array $config
+     * @param LoggerInterface|null $logger
      * @return Handler
      */
-    public static function provide(Client $client, $config = array(), LoggerInterface $logger = null);
+    public static function provide(Client $client, Session $session, $config = array(), LoggerInterface $logger = null);
 
 //    /**
 //     * @param Update $update
