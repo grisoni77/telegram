c8f959bb29328122df97f4a4459898d83f25fd12
Cristiano Cucco <cristiano.cucco@gmail.com>
2016-06-12 23:02:26 +0200

    added config to session factory

--- src/Session/SessionFactory.php
+++ src/Session/SessionFactory.php
@@ -18,9 +18,10 @@ abstract class SessionFactory
      * @param int $session_id
      * @param string $type Session handler type
      * @param string $token bot token
+     * @param array $config
      * @return Session
      */
-    public static function create($session_id, $type, $token)
+    public static function create($session_id, $type, $token, $config = array())
     {
         switch ($type)
         {
