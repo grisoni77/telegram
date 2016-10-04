1d696edc01cf17bcef8e3cab9e8ccefff64f644e
Cristiano Cucco <cristiano.cucco@gmail.com>
2016-10-04 09:23:33 +0200

    add phpunit bootstrap script

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__. '/../vendor/autoload.php';
+// autoloader
+$loader = new Symfony\Component\ClassLoader\UniversalClassLoader();
+// You can search the include_path as a last resort.
+//$loader->useIncludePath(true);
+// ... register namespaces and prefixes here - see below
+$loader->registerNamespaces(array(
+    'gr77' => __DIR__.'/..',
+    'tests' => __DIR__.'/..',
+));
+// run class loader
+$loader->register();
